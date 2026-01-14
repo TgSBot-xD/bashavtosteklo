f84e1a282846a4c07ed018222c28abf1eb0c521a
Daniil Soldanteko <falleytrd88@gmail.com>
2026-01-14 18:13:50 +0500

    error: Закоментирована TODO

--- src/widget/hero-section/ui/HeroSection.tsx
+++ src/widget/hero-section/ui/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { HeroContent } from './hero-content';
 import { HighlightsBlock } from './highlights-block';
 
-TODO [BAS-14]: Переделать вёрстку под динамические размеры
+// TODO: [BAS-14] Переделать вёрстку под динамические размеры
 export function HeroSection() {
 	return (
 		<main className="flex justify-around">
