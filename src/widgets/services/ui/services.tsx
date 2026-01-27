f244740f0c77fe11bb0953ff4eb37b6ef4a8ea3d
Daniil Soldanteko <falleytrd88@gmail.com>
2026-01-27 13:32:02 +0500

    feat: Добавлен hover для "услуг", изменены границы "услуг", перенесён useScrolled из header

--- src/widgets/services/ui/services.tsx
+++ src/widgets/services/ui/services.tsx
@@ -1,6 +1,4 @@
-import { Dot } from 'lucide-react';
-
-import { servicesList } from '../config/services-items';
+import { renderServicesList } from './services-items';
 
 import { Divider } from 'shared/ui';
 
@@ -19,45 +17,6 @@ function Services() {
       </div>
     </section>
   );
-}
-
-function renderServicesList() {
-  return servicesList.map(({ id, title, label, workingHours }) => {
-    return (
-      <div
-        key={id}
-        aria-label={title}
-        className="ring-foreground/10 bg-card flex flex-col rounded-xl p-4 ring"
-      >
-        <div className="flex flex-col gap-4">
-          <div className="flex items-center justify-between">
-            {/* Header секции услуг */}
-            <span className="text-foreground/90 text-base font-bold lg:text-lg">{title}</span>
-            <span className="ring-foreground/10 rounded-lg px-2 py-1 text-xs text-nowrap ring">
-              {workingHours}
-            </span>
-          </div>
-
-          <div className="flex w-full flex-col">
-            {/* Центральная часть секции услуг */}
-            <div className="flex flex-row items-center">
-              <Dot />
-              <span className="text-foreground/80 text-sm">{label[0]}</span>
-            </div>
-            <div className="flex flex-row items-center">
-              <Dot />
-              <span className="text-foreground/80 text-sm">{label[1]}</span>
-            </div>
-          </div>
-
-          <div className="flex flex-row">
-            {/* Нижняя часть секции услуг */}
-            <span className="text-foreground/80 flex text-xs lg:text-sm">Подробнее</span>
-          </div>
-        </div>
-      </div>
-    );
-  });
 }
 
 export { Services };
